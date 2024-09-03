bdbec2e326bf9896717eb4125a8e03bcf261283b
Shubham Gupta <shubham272101@gmail.com>
2024-09-03 21:43:29 +0530

    add new directory inside docker

---
+++ demoproject/demoproj1/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return <div className="flex flex-col justify-center h-screen">
+    <div className="flex justify-center">
+      <div className="text-3xl font-bold">
+        Hi there, My name is shubham gupta
+      </div>
+    </div>
+  </div>
+}
