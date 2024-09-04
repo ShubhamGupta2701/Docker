83b8e5ab256a6774be068a4a941d6989d912dc77
Shubham Gupta <shubham272101@gmail.com>
2024-09-04 14:28:40 +0530

    add new demo project directory and project

---
+++ demoproject/demoapp1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className='text-3xl font-mono font-bold'>
+          Hi there, Shubham this side
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
