2c298f2514076cf85e27b4242900f0a43b51dbd0
Shubham Gupta <shubham272101@gmail.com>
2024-09-04 14:18:27 +0530

    remove demoproj1

--- demoproject/demoproj1/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return <div className="flex flex-col justify-center h-screen">
-    <div className="flex justify-center">
-      <div className="text-3xl font-bold">
-        Hi there, My name is shubham gupta
-      </div>
-    </div>
-  </div>
-}
